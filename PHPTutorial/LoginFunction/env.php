6411c451a0319007ffa6596b36e0a8403e5f2f0f
Kona <kitami.kitano20@gmail.com>
2020-07-26 22:57:50 +0900

    form tutotial追加

---
+++ PHPTutorial/LoginFunction/env.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'users');
+define('DB_USER', 'kona');
+define('DB_PASS', 'konapasswd1234');
---
+++ PHPTutorial/form/blog.php
@@ -0,0 +1,9 @@
+<?php
+
+$blog = $_POST;
+
+foreach ($blog as $key => $value){
+    echo '<pre>';
+    echo $key . ' : ' . htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    echo '<pre>';
+}
