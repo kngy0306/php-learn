1101a34967342c5779dcfc9e49d8d1f6765412bd
Kona <kitami.kitano20@gmail.com>
2020-08-29 16:24:20 +0900

    functionを使用して処理をまとめる

--- PHPTutorial/PHP&MySQL_Tutorial/dbc.php
+++ PHPTutorial/PHP&MySQL_Tutorial/dbc.php
@@ -1,28 +1,61 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
-$user = 'blog_user';
-$pass = 'passwd';
+// 関数1つに1つの機能のみを持たせる -> testや管理のしやすさから
+// 1. データベース接続
+// 2. データ取得
+// 3. カテゴリ名表示
 
-try{
-  $dbh = new PDO($dsn,$user,$pass,[
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // error表示するためのモード
-  ]);
-  // echo '接続成功';
+// 1. DB接続
+// 引数 : なし
+// 返り値 : 接続結果
+function dbConnect()
+{
+  $dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
+  $user = 'blog_user';
+  $pass = 'passwd';
 
+  try {
+    $dbh = new PDO($dsn, $user, $pass, [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // error表示するためのモード
+    ]);
+  } catch (PDOException $e) {
+    echo '接続失敗' . $e->getMessage();
+    exit();
+  };
+
+  return $dbh;
+}
+// 2. データを取得
+// 引数 : なし
+// 返り値 : データ
+function getAllBlog()
+{
+  $dbh = dbConnect();
   // ①sqlの準備
   $sql = 'SELECT * FROM blog';
   // ②sqlの実行
   $stmt = $dbh->query($sql);
   // ③sqlの結果を受け取る
   $result = $stmt->fetchall(PDO::FETCH_ASSOC);
-
-  //dbの終了
+  return $result;
   $dbh = null;
-}catch(PDOException $e){
-  echo '接続失敗'. $e->getMessage();
-  exit();
-};
+}
+
+$blogData = getAllBlog();
+
+// 3. カテゴリ名を表示
+// 引数 : 数字
+// 返り値 : カテゴリ名
+function setCategoryName($category)
+{
+  if ($category === '1') {
+    return 'ブログ';
+  } elseif ($category === '2') {
+    return '日常';
+  } else {
+    return 'その他';
+  }
+}
 
 ?>
 
@@ -43,11 +76,11 @@ try{
       <th>タイトル</th>
       <th>カテゴリ</th>
     </tr>
-    <?php foreach($result as $column): ?>
+    <?php foreach ($blogData as $column) : ?>
     <tr>
       <td><?php echo $column['id'] ?></td>
       <td><?php echo $column['title'] ?></td>
-      <td><?php echo $column['category'] ?></td>
+      <td><?php echo setCategoryName(['category']) ?></td>
     </tr>
     <?php endforeach; ?>
   </table>
