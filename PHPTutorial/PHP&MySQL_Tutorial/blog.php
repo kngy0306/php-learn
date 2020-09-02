887e7cbcca961e402b44573bdbf03ebedf1c8909
Kona <kitami.kitano20@gmail.com>
2020-09-03 02:10:57 +0900

    ブログ更新処理追加

--- PHPTutorial/PHP&MySQL_Tutorial/blog.php
+++ PHPTutorial/PHP&MySQL_Tutorial/blog.php
@@ -41,4 +41,54 @@ class Blog extends Dbc
       $dbh->rollBack();
     }
   }
+
+  public function blogUpdate($blogs)
+  {
+    $sql = "UPDATE $this->table_name SET 
+              title = :title, content = :content, category = :category, publish_status = :publish_status
+            WHERE
+              id = :id";
+
+    $dbh = $this->dbConnect();
+    $dbh->beginTransaction();
+
+    try {
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindValue(':title', $blogs['title'], PDO::PARAM_STR);
+      $stmt->bindValue(':content', $blogs['content'], PDO::PARAM_STR);
+      $stmt->bindValue(':category', $blogs['category'], PDO::PARAM_INT);
+      $stmt->bindValue(':publish_status', $blogs['publish_status'], PDO::PARAM_INT);
+      $stmt->bindValue(':id', $blogs['id'], PDO::PARAM_INT);
+      $stmt->execute();
+      $dbh->commit();
+
+      echo 'ブログを更新しました！';
+    } catch (PDOException $e) {
+      exit($e);
+      $dbh->rollBack();
+    }
+  }
+
+  // ブログのバリデーション
+  function blogValidate($blogs)
+  {
+    if (empty($blogs['title'])) {
+      exit('タイトルを入力してください');
+    }
+    if (mb_strlen($blogs['title']) > 191) {
+      exit('タイトルは191文字以内にしてください');
+    }
+
+    if (empty($blogs['content'])) {
+      exit('本文を入力してください');
+    }
+
+    if (empty($blogs['category'])) {
+      exit('カテゴリーを選択してください');
+    }
+
+    if (empty($blogs['publish_status'])) {
+      exit('公開、非公開を選択してください');
+    }
+  }
 }
--- PHPTutorial/PHP&MySQL_Tutorial/dbc.php
+++ PHPTutorial/PHP&MySQL_Tutorial/dbc.php
@@ -1,5 +1,4 @@
 <?php
-
 class Dbc
 {
   protected $table_name;
